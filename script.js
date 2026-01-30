0a859182519384cdceb8bc5abf04d287e1c8977a
goofy9901 <borissavicc789@gmail.com>
2026-01-30 16:03:57 +0100

    Implementing Computer and Human choices

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log(5);
+let getComputerChoice = function () {
+  let randomNumber = Math.floor(Math.random() * 3);
+
+  return randomNumber === 0
+    ? "rock"
+    : randomNumber === 1
+      ? "paper"
+      : "scissors";
+};
+console.log(getComputerChoice());
+
+let getHumanChoice = function () {
+  let humanNumber = prompt("Choose rock, paper or scissors");
+  return humanNumber;
+};
+
+//   if (humanNumber === "rock") {
+//     return "rock";
+//   } else if (humanNumber === "paper") {
+//     return "paper";
+//   } else if (humanNumber === "scissors") {
+//     return "scissors";
+//   }
+// };
+
+console.log(getHumanChoice());
