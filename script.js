39006cf2a48cf19032912785eac6ef97caa93a74
goofy9901 <borissavicc789@gmail.com>
2026-02-02 13:12:06 +0100

    Implementing the first round of the game

--- script.js
+++ script.js
@@ -23,4 +23,31 @@ let getHumanChoice = function () {
 //   }
 // };
 
-console.log(getHumanChoice());
+let computerScore = 0;
+let humanScore = 0;
+
+let playRound = function (humanChoice, computerChoice) {
+  let hC = humanChoice.toLowerCase();
+  let cC = computerChoice.toLowerCase();
+
+  if (hC === cC) {
+    console.log("It's a tie");
+  } else if (
+    (hC === "rock" && cC === "scissors") ||
+    (hC === "paper" && cC === "rock") ||
+    (hC === "scissors" && cC === "paper")
+  ) {
+    humanScore++;
+    console.log(`You win ${hC} beats ${cC}`);
+  } else {
+    computerScore++;
+    console.log(`You lose ${cC} beats ${hC}`);
+  }
+};
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
+
+let playGame = function () {};
